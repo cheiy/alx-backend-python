388b9fa602b4772c404c2e2e268626f9a4ad0f29
cheiy <gschege@gmail.com>
2023-08-10 17:24:36 +0000

    Sorted

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,14 +4,15 @@ Module contains an async routine called wait_n that takes in 2 in arguments
 (n, max_delay). It then spawns wait_random n times with the specified max_delay
 wait_n returns the list of all the delays in ascending order.
 """
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Function spawns wait_random n times
     """
     times_list: float = []
-    for i in range(n: int):
+    for i in range(n):
         times_list.append(await wait_random(max_delay))
-    return float(times_list)
+    return sorted(times_list)
