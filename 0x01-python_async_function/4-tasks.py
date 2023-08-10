6eccfc873f06077de92f2adeca406f7ea2be67d1
cheiy <gschege@gmail.com>
2023-08-10 22:20:38 +0000

    Annotation

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,7 +12,7 @@ async def task_wait_n(n: int, max_delay: float) -> List[float]:
     Function calls the task_wait_random async function and returns
     a list of times
     """
-    times_list = []
+    times_list: list = []
     for i in range(n):
         task = await task_wait_random(max_delay)
         times_list.append(task)
