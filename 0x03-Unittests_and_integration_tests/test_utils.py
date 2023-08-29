bebb9ed58f3a858af4caacfeebaba7062749d65f
cheiy <gschege@gmail.com>
2023-08-29 00:34:25 +0000

    Exception raising

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,3 +27,18 @@ class TestAccessNestedMap(unittest.TestCase):
         Method tests access_nested_map function output
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            exception: Exception,
+            ) -> None:
+        '''Tests `access_nested_map`'s exception raising.
+        '''
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
